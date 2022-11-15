e5d0de764fbc06d33a2c0c609d2c5c5ce8a23979
HauwIsLife <118190931+HauwIsLife@users.noreply.github.com>
2022-11-15 20:16:45 +0630

    Update Project.py

--- Project.py
+++ Project.py
@@ -3,6 +3,6 @@ name = input("Hi, what is your name?")
 # Mathaias was here
 
 if name == "Mathew":
-  print("hello mathaias johnson the third"
+  print("hello mathaias johnson the third")
 else:
   print("Hi " + name +", Welcome to Python!")
