5b5fc3107040fc332ae97972e61a89bddd7c6fff
Mathew <73152504+NotMathew@users.noreply.github.com>
2022-11-16 17:16:49 +0700

    Added some stuff

--- Chemistry Project.py
+++ Chemistry Project.py
@@ -1,3 +1,17 @@
+print("By Nathan, Mathew, and Willbert")
+print("1. Gol_IA                            9. Gol_IB")
+print("2. Gol_IIA                           10. Gol_IIB")
+print("3. Gol_IIIB                          11. Gol_IIIA")
+print("4. Gol_IVB                           12. Gol_IVA")
+print("5. Gol_VB                            13. Gol_VA")
+print("6. Gol_VIB                           14. Gol_VIA")
+print("7. Gol_VIIB                          15. Gol_VIIA")
+print("8. Gol_VIIIB                         16. Gol_VIIIA")
+print("Please select what golongan do you want to explore:")
+input1 = input()
+
+
+
 M = ["Li","Na","K","Rb","Cs","Fr","Be","Mg","Ca","Sr","Ba","Ra","B","Al","Ga","In","Tl","Si","Ge","Sn","Pb","As","Sb","Bi","Te","Po",",At"]
 Non_M = ["H","C","N","P","O","S","Se","F","Cl","Br","I","He","Ne","Ar","Kr","Xe","Rn"]
 # Net, di line 1 dan 2 itu atom apaan?
