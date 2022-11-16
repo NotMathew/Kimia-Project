77b55206b417a791d5c87499c2d43edf05dd26f5
Mathew <73152504+NotMathew@users.noreply.github.com>
2022-11-16 18:47:27 +0700

    Ive no idea what to add

--- Chemistry Project.py
+++ Chemistry Project.py
@@ -1,3 +1,8 @@
+# Imports
+from time import sleep
+import random
+
+# Main
 print("By Nathan, Mathew, and Willbert")
 print("1. Gol_IA                            9.  Gol_IB")
 print("2. Gol_IIA                           10. Gol_IIB")
@@ -7,9 +12,36 @@ print("5. Gol_VB                            13. Gol_VA")
 print("6. Gol_VIB                           14. Gol_VIA")
 print("7. Gol_VIIB                          15. Gol_VIIA")
 print("8. Gol_VIIIB                         16. Gol_VIIIA")
-print("Please select what golongan do you want to explore:")
+print("Please type the number what golongan do you want to explore:")
 input1 = input()
-# input1_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]
+
+if input1 == ("1"):
+    print("Itu ikatan Ion")
+else:
+    print("Saya tidak tahu")
+    sleep(3)
+    print("I'll think for you")
+    sleep(2)
+    print("5")
+    sleep(1)
+    print("4")
+    sleep(1)
+    print("3")
+    sleep(1)
+    print("2")
+    sleep(1)
+    print("1")
+    sleep(1)
+    print("0")
+
+
+
+    counter = 0
+    while counter < 2:
+      print("Loading...")
+      counter =+ 1
+
+
 
 
 M = ["Li","Na","K","Rb","Cs","Fr","Be","Mg","Ca","Sr","Ba","Ra","B","Al","Ga","In","Tl","Si","Ge","Sn","Pb","As","Sb","Bi","Te","Po",",At"]
@@ -27,14 +59,14 @@ Col_VIIB = ["F","Cl","Br","I","At"]
 Col_VIIIB = ["He","Ne","Ar","Kr","Xe","Rn"]
 
 from time import sleep
-print("Welcome to the limited periodic table bonding dictionary")
+#print("Welcome to the limited periodic table bonding dictionary")
 # sleep(2)
-print("Insert 2 atoms from column IA-VIIA")
+#print("Insert 2 atoms from column IA-VIIA")
 # sleep(2)
-atom_1 = input("1st Atom = ")
-atom_2 = input("2nd Atom = ")
+#atom_1 = input("1st Atom = ")
+#atom_2 = input("2nd Atom = ")
 
-if atom_1 in M and atom_2 in Non_M:
-    print("This is an Ion Bond")
-else:
-    print("Invalid atom")
+#if atom_1 in M and atom_2 in Non_M:
+#    print("This is an Ion Bond")
+#else:
+#    print("Invalid atom")
