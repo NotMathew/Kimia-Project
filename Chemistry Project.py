c7ad521d7a31220a371cda68878dd08e0a3d436d
Dogsarefunnyxd <74320172+Dogsarefunnyxd@users.noreply.github.com>
2022-11-16 19:33:31 +0700

    Updated
    Added More Groups From The Periodic Table
    Group IB - VIIIB10
    
    Fixed some small details.

--- Chemistry Project.py
+++ Chemistry Project.py
@@ -3,24 +3,26 @@ from time import sleep
 import random
 
 # Main
-print("By Nathan, Mathew, and Willbert")
+print("By Nathan, Mathew, and Wilbert")
 print("1. Gol_IA                            9.  Gol_IB")
 print("2. Gol_IIA                           10. Gol_IIB")
-print("3. Gol_IIIB                          11. Gol_IIIA")
-print("4. Gol_IVB                           12. Gol_IVA")
-print("5. Gol_VB                            13. Gol_VA")
-print("6. Gol_VIB                           14. Gol_VIA")
-print("7. Gol_VIIB                          15. Gol_VIIA")
-print("8. Gol_VIIIB                         16. Gol_VIIIA")
-print("Please type the number what golongan do you want to explore:")
+print("3. Gol_IIIA                          11. Gol_IIIB")
+print("4. Gol_IVA                           12. Gol_IVB")
+print("5. Gol_VA                            13. Gol_VB")
+print("6. Gol_VIA                           14. Gol_VIB")
+print("7. Gol_VIIA                          15. Gol_VIIB")
+print("8. Gol_VIIIA                         16. Gol_VIIIB8")
+print("                                     17. Gol_VIIIB9")
+print("                                     18. Gol_VIIIB10")
+print("Please type the number to start exploring the world of chemistry")
 input1 = input()
-
+#Correct it if it's wrong - Wilbert
 if input1 == ("1"):
-    print("Itu ikatan Ion")
+    print("That is an ionic bond.")
 else:
     print("Saya tidak tahu")
     sleep(3)
-    print("I'll think for you")
+    print("I'll help you")
     sleep(2)
     print("5")
     sleep(1)
@@ -42,26 +44,37 @@ else:
       counter =+ 1
 
 
+#Correct it if it's wrong - Wilbert
 
-
-M = ["Li","Na","K","Rb","Cs","Fr","Be","Mg","Ca","Sr","Ba","Ra","B","Al","Ga","In","Tl","Si","Ge","Sn","Pb","As","Sb","Bi","Te","Po",",At"]
+M = ["Li","Na","K","Rb","Cs","Fr","Be","Mg","Ca","Sr","Ba","Ra","B","Al","Ga","In","Tl","Si","Ge","Sn","Pb","As","Sb","Bi","Te","Po",",At","Cu","Ag","Au","Uuu","Zn","Cd"]
+M = ["Hg","Uub","Sc","Y","La","Ac","Ti","Zr","Hf","Rf","V","Nb","Ta","Db","Cr","Mo","W","Sg","Mn","Tc","Re","Bh","Fe","Ru","Os","Hs","Co","Rh","Ir","Mt","Ni","Pd","Pt","Ds"]
 Non_M = ["H","C","N","P","O","S","Se","F","Cl","Br","I","He","Ne","Ar","Kr","Xe","Rn"]
 # Net, di line 1 dan 2 itu atom apaan?
 # Metal dan Non-Metal?
 
 Col_IA = ["H","li","Na","K","Rb","Cs","Fr"]
 Col_IIA = ["Be","Mg","Ca","Sr","Ba","Ra"]
-Col_IIIB = ["B","Al","Ga","In","Tl"]
-Col_IVB = ["C","Si","Ge","Sn","Pb"]
-Col_VB = ["N","P","As","Sb","Bi"]
-Col_VIB = ["O","S","Se","Te","Po"]
-Col_VIIB = ["F","Cl","Br","I","At"]
-Col_VIIIB = ["He","Ne","Ar","Kr","Xe","Rn"]
+Col_IIIA = ["B","Al","Ga","In","Tl"]
+Col_IVA = ["C","Si","Ge","Sn","Pb"]
+Col_VA = ["N","P","As","Sb","Bi"]
+Col_VIA = ["O","S","Se","Te","Po"]
+Col_VIIA = ["F","Cl","Br","I","At"]
+Col_VIIIA = ["He","Ne","Ar","Kr","Xe","Rn"]
+Col_IB = ["Cu","Ag","Au","Uuu"]
+Col_IIB = ["Zn","Cd","Hg","Uub"]
+Col_IIIB = ["Sc","Y","La","Ac"]
+Col_IVB = ["Ti","Zr","Hf","Rf"]
+Col_VB = ["V","Nb","Ta","Db"]
+Col_VIB = ["Cr","Mo","W","Sg"]
+Col_VIIB = ["Mn","Tc","Re","Bh"]
+Col_VIIIB8 = ["Fe","Ru","Os","Hs"]
+Col_VIIIB9 = ["Co","Rh","Ir","Mt"]
+Col_VIIIB10 = ["Ni","Pd","Pt","Ds"]
 
 from time import sleep
 #print("Welcome to the limited periodic table bonding dictionary")
 # sleep(2)
-#print("Insert 2 atoms from column IA-VIIA")
+#print("Insert 2 atoms from column IA-VIIIB10")
 # sleep(2)
 #atom_1 = input("1st Atom = ")
 #atom_2 = input("2nd Atom = ")
