6dfb7943d50dd4ca3a1a9c32e9c2c7142b3d72c1
Mathew <73152504+NotMathew@users.noreply.github.com>
2022-11-16 09:34:11 +0700

    LOLLLLL

--- Project.py
+++ Project.py
@@ -1,12 +1,25 @@
-name = input("Hi, what is your name?")
-
-# Mathaias was here
-# LOLE
-# Click Commit and Push untuk ohm Mathew
-# ^_^
-# Update me
-
-if name == "Mathew":
+from time import sleep
+name = input("Hi, what is your name?").lower()
+if name == "mathew":
   print("hello mathaias johnson the third")
+elif name == "nathan":
+  investigator = input("R u shure about THAR????(y/n)").lower()
+  if  investigator == "y":
+    print("0_., Lolez welcome nathan the third")
+  else:
+    print("0_0")
+    sleep(2)
+    print("BYe Bye, nice try. you lied to me")
+    sleep(20)
+    print("You still here?")
+    sleep(2)
+    print("Bye for real")
+    sleep(2)
+    print("~Nathan")
+    exit()
 else:
   print("Hi " + name +", Welcome to Python!")
+
+
+gol1 = ["Hidrogen", ""]
+
