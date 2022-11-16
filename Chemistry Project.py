e4f7d75412fcc9b8e306e2abb5c84270c6590720
Mathew <73152504+NotMathew@users.noreply.github.com>
2022-11-16 16:48:25 +0700

    Tambah sekidik details

--- Chemistry Project.py
+++ Chemistry Project.py
@@ -1,14 +1,14 @@
 M = ["Li","Na","K","Rb","Cs","Fr","Be","Mg","Ca","Sr","Ba","Ra","B","Al","Ga","In","Tl","Si","Ge","Sn","Pb","As","Sb","Bi","Te","Po",",At"]
 Non_M = ["H","C","N","P","O","S","Se","F","Cl","Br","I","He","Ne","Ar","Kr","Xe","Rn"]
 
-Col_I = ["H","li","Na","K","Rb","Cs","Fr"]
-Col_II = ["Be","Mg","Ca","Sr","Ba","Ra"]
-Col_III = ["B","Al","Ga","In","Tl"]
-Col_IV = ["C","Si","Ge","Sn","Pb"]
-Col_V = ["N","P","As","Sb","Bi"]
-Col_VI = ["O","S","Se","Te","Po"]
-Col_VII = ["F","Cl","Br","I","At"]
-Col_VIII = ["He","Ne","Ar","Kr","Xe","Rn"]
+Col_IA = ["H","li","Na","K","Rb","Cs","Fr"]
+Col_IIA = ["Be","Mg","Ca","Sr","Ba","Ra"]
+Col_IIIB = ["B","Al","Ga","In","Tl"]
+Col_IVB = ["C","Si","Ge","Sn","Pb"]
+Col_VB = ["N","P","As","Sb","Bi"]
+Col_VIB = ["O","S","Se","Te","Po"]
+Col_VIIB = ["F","Cl","Br","I","At"]
+Col_VIIIB = ["He","Ne","Ar","Kr","Xe","Rn"]
 
 from time import sleep
 print("Welcome to the limited periodic table bonding dictionary")
