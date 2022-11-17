17134860e2deb723813b458087c13d73cf052db6
Mathew <73152504+NotMathew@users.noreply.github.com>
2022-11-17 10:36:15 +0700

    revised the inputs

--- Chemistry Project.py
+++ Chemistry Project.py
@@ -42,7 +42,8 @@ sleep(1)
 print("Hello and welcome to the simple element Dictionary or Simulation")
 sleep(1)
 while running == True:
-    user_input = input("Dictionary or Simulation?").lower()
+    print("Dictionary or Simulation?")
+    user_input = input("").lower()
     if user_input == "dictionary":
         dictionary_running = True
 
@@ -111,20 +112,36 @@ while running == True:
             sleep(1)
             atom_1 = input("1st Atom = ")
             atom_2 = input("2nd Atom = ")
-            if atom_1 or atom_2 == "home":
-                break
-            elif atom_1 in M and atom_2 in Non_M or atom_1 in Non_M and atom_2 in M:
+            if atom_1 in M and atom_2 in Non_M or atom_1 in Non_M and atom_2 in M:
                 print("This is an Ion Bond")
                 sleep(1)
-                print("It happens between a Metalic atom and a Non Metalic atom")
+                print("It occurs between a Metalic atom and a Non Metalic atom")
+                sleep(1)
+                print("You typed " + atom_1 + "and " + atom_2)
                 sleep(1)
                 print("This bond involves giving and taking between the electrons of those atoms")
+                sleep(1)
+                print("The difference in electronegativity between the two atoms are more or equal to 1.7")
             elif atom_1 in Non_M and atom_2 in Non_M:
                 print("This is a Covalent Bond")
-                print("It happens between two Non Metalic atoms")
+                sleep(1)
+                print("It occurs between two Non Metalic atoms")
+                sleep(1)
+                print("You typed " + atom_1 + "and " + atom_2)
+                sleep(1)
+                print("This bond involves the sharing of electrons between the atoms")
+                sleep(1)
+                print("The difference in electronegativity between the two atoms are less than 1.7")
             elif atom_1 in M and atom_2 in M:
                 print("This is a Metalic Bond")
-                print("It happens between two Metalic atoms")
+                sleep(1)
+                print("It occurs between two Metalic atoms")
+                sleep(1)
+                print("You typed " + atom_1 + "and " + atom_2)
+                sleep(1)
+                print("This bond involves the sharing of electrons between many metalic atoms")
+                sleep(1)
+                print("The metal is held together by strong forces of attraction between delocalized electrons and positive ions")
             else:
                  print("Invalid atom or input")
 
